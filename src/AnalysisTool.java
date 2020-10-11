38aff91d771774ebe1e5795d120843c1d01c0833
rosswilliamson1 <rossw66@sky.com>
2020-10-11 15:19:23 +0100

    Finished WMC with Cyclomatic Complexity

--- src/AnalysisTool.java
+++ src/AnalysisTool.java
@@ -1,12 +1,19 @@
+import com.github.javaparser.Position;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.ClassExpr;
+import com.github.javaparser.ast.expr.ConditionalExpr;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.stmt.*;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 public class AnalysisTool {
 
@@ -14,11 +21,18 @@ public class AnalysisTool {
     private static ArrayList<ClassOrInterfaceDeclaration> classes = new ArrayList<>();
     private static ArrayList<MethodDeclaration> methods = new ArrayList<>();
     private static HashMap<ClassOrInterfaceDeclaration, Integer> simpleComplexities = new HashMap<>();
+    private static int complexityRunning;
+    private static int finalClassLine;
+    private static ArrayList<Integer> cyclomaticComplexity = new ArrayList<>();
+    private static Optional classRange = Optional.empty();
+
+
 
     public static void main(String[] args) throws Exception {
 
         getFiles();
         CompilationUnit cu;
+
 
         for (File f: files) {
             System.out.println("-------NEW FILE: " + f.getName() +"--------");
@@ -49,20 +63,22 @@ public class AnalysisTool {
     }
 
     private static void calculateComplexComplexity() {
-        for (ClassOrInterfaceDeclaration c: classes) {
-            List<MethodDeclaration> methodList = c.getMethods();
-            int totalComplexity = 0;
-            for (MethodDeclaration m : methodList) {
-                int complexity = 1;
+        System.out.println("---------COMPLEX CLASS COMPLEXITY----------");
+        int classIndex=0;
+        int classComplexity = cyclomaticComplexity.get(classIndex);
+        for (File f : files) {
+            if (classIndex != 0) {
+                classComplexity = cyclomaticComplexity.get(classIndex) - cyclomaticComplexity.get(classIndex - 1);
             }
-
-
+            System.out.println("Class Name: "+f.getName());
+            System.out.println("Cyclomatic Complexity: "+classComplexity);
+            classIndex++;
         }
     }
 
     private static void getFiles() {
        File[] folderFiles;
-       File in = new File("/home/heather/409groupassignment/Animal");
+       File in = new File("Animal");
        folderFiles = in.listFiles();
        for (File f : folderFiles) {
             if (f.getName().endsWith(".java")) {
@@ -76,13 +92,83 @@ public class AnalysisTool {
 
         public void visit(ClassOrInterfaceDeclaration ci, Object a) {
             classes.add(ci);
+            //cyclomaticComplexity.add(complexityRunning);
+            classRange = ci.getRange();
+            finalClassLine = parseRange(classRange);
             super.visit(ci, a);
         }
 
         public void visit(MethodDeclaration m, Object a) {
             methods.add(m);
+            Optional p = m.getRange();
+            int methodRange = parseRange(p);
+            ArrayList<Integer> finalClassLines = makeLineList(finalClassLine);
+            if (finalClassLines.contains(methodRange)) {
+                cyclomaticComplexity.add(complexityRunning);
+            }
             super.visit(m, a);
         }
+
+        public void visit(MethodCallExpr m, Object a) {
+            complexityRunning++;
+            super.visit(m, a);
+        }
+
+        public void visit(ConditionalExpr c, Object a) {
+            complexityRunning++;
+            super.visit(c, a);
+        }
+
+        public void visit(ForEachStmt fe, Object a) {
+            complexityRunning++;
+            super.visit(fe, a);
+        }
+
+        public void visit(ForStmt f, Object a) {
+            complexityRunning++;
+            super.visit(f, a);
+        }
+
+        public void visit(WhileStmt w, Object a) {
+            complexityRunning++;
+            super.visit(w, a);
+        }
+
+        public void visit(DoStmt d, Object a) {
+            complexityRunning++;
+            super.visit(d, a);
+        }
+
+        public void visit(SwitchEntry se, Object a) {
+            complexityRunning++;
+            super.visit(se, a);
+        }
+
+        public void visit(BinaryExpr be, Object a) {
+            BinaryExpr.Operator operator = be.getOperator();
+            if (operator.asString().equals("AND") || operator.asString().equals("OR")) {
+                complexityRunning++;
+            }
+            super.visit(be, a);
+        }
+
+        public int parseRange(Optional r) {
+            String s = r.toString();
+            int lineIndex = s.indexOf("line", s.indexOf("line")+1);
+            int numIndex = lineIndex + 5;
+            int spaceIndex = s.indexOf(",", numIndex);
+            String lineNumber = s.substring(numIndex, spaceIndex);
+            return Integer.parseInt(lineNumber);
+        }
+
+        public ArrayList<Integer> makeLineList(int l) {
+            ArrayList<Integer> lineList = new ArrayList();
+            lineList.add(l);
+            lineList.add(l-1);
+            return lineList;
+        }
+
+
     }
 
 }
