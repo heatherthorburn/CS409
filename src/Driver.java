80088434734021f284279ebf26b256b2dbb23645
heatherthorburn <44814619+heatherthorburn@users.noreply.github.com>
2020-10-15 00:49:42 +0100

    Delete Driver.java

--- src/Driver.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class Driver {
-    public static void main (String[] args) throws IOException {
-        ProjectAnalysis p = new ProjectAnalysis();
-        p.start();
-    }
-}
